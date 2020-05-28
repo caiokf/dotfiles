509aa6c84f6ae46e3d72d0263d3f8b460ebe6827
Caio Kinzel Filho <caiokf@gmail.com>
2020-05-28 19:48:57 -0300

    Adding node global packages config

---
+++ node/install.sh
@@ -0,0 +1,22 @@
+# working with npm
+npm install -g yarn
+npm install -g pnpm
+npm install -g npx
+npm install -g npm-check-updates
+npm install -g np
+
+# debugging
+npm install -g ndb
+npm install -g node-inspector
+
+# general utilities
+npm install -g vtop
+npm install -g tldr
+npm install -g fkill-cli
+
+# mobile
+npm install -g react-native-cli
+npm install -g expo-cli
+
+# typescript
+npm install -g typescript
